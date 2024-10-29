0bd889a36bf0395b18ab9b949497abc41c1f5757
nguyenhoanghieu <joseph.nguyenhoanghieu@gmail.com>
2024-10-29 13:19:01 +0700

    hieu

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -14,9 +14,10 @@ namespace ToDoList
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Console.WriteLine("hieu");
         }
     }
 }
