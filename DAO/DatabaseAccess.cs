14fa24e4b1318c20c14df84d2b7bdbbb997a6cc6
hualapthanh <hualapthanh@gmail.com>
2024-11-12 22:04:39 +0700

    Thêm DatabaseAccess

---
+++ DAO/DatabaseAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAO
+{
+    public class SqlConnectionData
+    {
+        public static SqlConnection Connect()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionStringName"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
+            return conn;
+        }
+    }
+    public class DatabaseAccess
+    {
+    }
+}
