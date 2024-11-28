89b986d147467917c33fd9cd39035b9eae733cd2
hualapthanh <hualapthanh@gmail.com>
2024-11-28 22:22:09 +0700

    Thanh - 28/11 - Delete FileName field

--- DTO/AttachmentDTO.cs
+++ DTO/AttachmentDTO.cs
@@ -11,23 +11,21 @@ namespace DTO
         public int AttachmentID { get; set; }
         public int TaskID { get; set; }
         public string FilePath { get; set; }
-        public string FileName { get; set; }
-        public string UploadedBy { get; set; }
+        public int UploadedBy { get; set; }
 
         public AttachmentDTO() { }
 
-        public AttachmentDTO(int attachmentID, int taskID, string filePath, string fileName, string uploadedBy)
+        public AttachmentDTO(int attachmentID, int taskID, string filePath, int uploadedBy)
         {
             AttachmentID = attachmentID;
             TaskID = taskID;
             FilePath = filePath;
-            FileName = fileName;
             UploadedBy = uploadedBy;
         }
 
         public override string ToString()
         {
-            return $"AttachmentDTO [AttachmentID={AttachmentID}, TaskID={TaskID}, FilePath={FilePath}, FileName={FileName}, UploadedBy={UploadedBy}]";
+            return $"AttachmentDTO [AttachmentID={AttachmentID}, TaskID={TaskID}, FilePath={FilePath}, UploadedBy={UploadedBy}]";
         }
 
         public override bool Equals(object obj)
@@ -41,7 +39,6 @@ namespace DTO
             return AttachmentID == other.AttachmentID &&
                    TaskID == other.TaskID &&
                    FilePath == other.FilePath &&
-                   FileName == other.FileName &&
                    UploadedBy == other.UploadedBy;
         }
 
@@ -51,7 +48,6 @@ namespace DTO
             hash = hash * 31 + AttachmentID.GetHashCode();
             hash = hash * 31 + TaskID.GetHashCode();
             hash = hash * 31 + (FilePath != null ? FilePath.GetHashCode() : 0);
-            hash = hash * 31 + (FileName != null ? FileName.GetHashCode() : 0);
             hash = hash * 31 + (UploadedBy != null ? UploadedBy.GetHashCode() : 0);
             return hash;
         }
